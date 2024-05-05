03d75907c42b89110fda887db8ad7e63e8d1b765
FranciscoJSB12 <javif8@gmail.com>
2024-05-05 13:02:06 -0400

    feat: Apply useSafeAreaInsets in profile screen

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,9 +1,25 @@
 import { View, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
 
 export const ProfileScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Profile</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 20,
+        marginTop: top,
+      }}
+    >
+      <Text style={{ marginBottom: 10 }}>Profile</Text>
+      <PrimaryButton
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}
+        label='Abrir menú'
+      />
     </View>
   );
 };
