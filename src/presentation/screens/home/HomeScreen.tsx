bebbb82f775a7cec46440f72727c9d532f7cd94b
FranciscoJSB12 <javif8@gmail.com>
2024-05-05 11:57:47 -0400

    feat: Add custom button to toggle drawer

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,28 @@
-import { View } from 'react-native';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { Pressable, View, Text } from 'react-native';
+import {
+  DrawerActions,
+  NavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
 import { globalStyles } from '../../theme/theme';
 import { PrimaryButton } from '../../components/shared/PrimaryButton';
 import { RootStackParams } from '../../routes/StackNavigator';
+import { useEffect } from 'react';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}
+        >
+          <Text>Menú</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
 
   return (
     <View style={globalStyles.container}>
