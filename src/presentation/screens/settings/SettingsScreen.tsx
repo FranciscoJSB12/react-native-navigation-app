82dfc6348b84fbeedeb2d3ca3d286cdc6e39b8b1
FranciscoJSB12 <javif8@gmail.com>
2024-05-05 11:31:12 -0400

    feat: Add popToTop functionality

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,5 +1,22 @@
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { StackActions, useNavigation } from '@react-navigation/native';
+import { globalStyles } from '../../theme/theme';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
 
 export const SettingScreen = () => {
-  return <Text>SettingsScreen</Text>;
+  const navigator = useNavigation();
+
+  return (
+    <View style={globalStyles.container}>
+      <Text style={{ marginBottom: 10 }}>Settings screen</Text>
+      <PrimaryButton
+        label='Regresar'
+        onPress={() => navigator.goBack()}
+      />
+      <PrimaryButton
+        label='Inicio'
+        onPress={() => navigator.dispatch(StackActions.popToTop())}
+      />
+    </View>
+  );
 };
