b7511ca1a03a5f435aea428f0046c2c51f72b69a
FranciscoJSB12 <javif8@gmail.com>
2024-05-05 16:27:39 -0400

    fix: remove code that is being unused

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,14 +1,9 @@
-import { Pressable, View, Text } from 'react-native';
-import {
-  DrawerActions,
-  NavigationProp,
-  useNavigation,
-} from '@react-navigation/native';
+import { View } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { HamburgerMenu } from '../../components/shared/HamburgerMenu';
 import { globalStyles } from '../../theme/theme';
 import { PrimaryButton } from '../../components/shared/PrimaryButton';
 import { RootStackParams } from '../../routes/StackNavigator';
-import { useEffect } from 'react';
 
 export const HomeScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
